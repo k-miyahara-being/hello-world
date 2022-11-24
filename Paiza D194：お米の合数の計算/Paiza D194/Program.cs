b8d5b27a8d8c592c06fb9e865262fb580b3b54fd
k-miyahara-being <k-miyahara@beingcorp.co.jp>
2022-11-24 19:39:10 +0900

    test2

---
+++ Paiza D194：お米の合数の計算/Paiza D194/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Paiza_D194 {
+    class Program {
+        static void Main(string[] args) {
+            //1 合あたり 540 kcalとして1日に食べるお米の量 n 合が与えられるので何 kcal摂取するのかを出力してください。
+
+            //1回目の提出
+            //var n = int.Parse(Console.ReadLine());//入力した整数を読み込む            
+            //Console.WriteLine("{0}",n * 540);//フォーマット出力」
+
+            //2回目の提出
+            //if(double.TryParse(Console.ReadLine(), out double n) == true && n >= 0){
+            //    Console.WriteLine($"{n * 540}");//文字列補間
+            //}
+            //else if(n < 0) {
+            //    Console.WriteLine("正の値を入力してください");
+            //}
+            //else{
+            //    Console.WriteLine("正の数字を入力してください。");
+            //}
+
+            //3回目の提出
+            if(!double.TryParse(Console.ReadLine(), out double n)){
+                Console.WriteLine("数字を入力してください");
+                return;
+            }
+            if(n < 0){
+                Console.WriteLine("正の数字を入力してください");
+            }
+            if(n >= 0){
+                Console.WriteLine($"{n * 540}");
+            }
+        }
+    }
+}
